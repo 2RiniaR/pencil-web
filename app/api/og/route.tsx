b8c1377a20cb801e91c1b4c2043fd4fb29302203
watano1168 <watano1168@gmail.com>
2024-02-25 05:31:17 +0900

    サンプルテスト

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -1,110 +1,31 @@
 import { ImageResponse } from "next/og";
-import { siteDomain, siteName, siteUrl } from "~/libs/const";
+// App router includes @vercel/og.
+// No need to install it.
 
 export const runtime = "edge";
 
-type Props = {
-  params: {
-    title: string;
-    thumbnail: string | undefined;
-  };
-};
-
-export async function GET(request: never, { params }: Props) {
-  return new ImageResponse(<OgImage title={params.title} thumbnail={params.thumbnail} />, {
-    width: 1200,
-    height: 630
-  });
-}
-
-type OgImageProps = {
-  title: string;
-  thumbnail: string | undefined;
-};
-
-const OgImage = ({ title, thumbnail }: OgImageProps) => (
-  <div
-    style={{
-      display: "flex",
-      position: "relative",
-      background: "linear-gradient(to bottom, #8791a3 0%, #6c717a 100%)",
-      width: 1200,
-      height: 630,
-      color: "#dddddd",
-      border: "solid 24px rgba(221, 221, 221, 0.7)",
-      fontFamily: '"m-plus-2c"',
-      fontWeight: 500,
-      fontStyle: "normal"
-    }}
-  >
-    <img
-      style={{
-        position: "absolute",
-        width: 416,
-        height: 234,
-        left: 368,
-        top: 70,
-        border: "solid 4px rgba(221, 221, 221, 0.7)",
-        borderRadius: 4
-      }}
-      src={thumbnail ?? `${siteUrl}/default_thumbnail.png`}
-      alt={title}
-    />
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: 42,
-        position: "absolute",
-        width: 800,
-        height: 100,
-        top: 330,
-        left: 176
-      }}
-    >
-      <p
+export async function GET() {
+  return new ImageResponse(
+    (
+      <div
         style={{
-          margin: 0,
-          textAlign: "center"
+          fontSize: 40,
+          color: "black",
+          background: "white",
+          width: "100%",
+          height: "100%",
+          padding: "50px 200px",
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center"
         }}
       >
-        {title}
-      </p>
-    </div>
-    <img
-      style={{
-        position: "absolute",
-        borderRadius: 1000,
-        width: 160,
-        height: 160,
-        left: -32,
-        bottom: -32
-      }}
-      src={`${siteUrl}/icon.jpg`}
-      alt="アイコン"
-    />
-    <div
-      style={{
-        fontSize: 32,
-        position: "absolute",
-        width: 400,
-        left: 148,
-        bottom: 16
-      }}
-    >
-      {siteName}
-    </div>
-    <div
-      style={{
-        fontSize: 32,
-        position: "absolute",
-        right: 16,
-        bottom: 16,
-        textAlign: "right"
-      }}
-    >
-      {siteDomain}
-    </div>
-  </div>
-);
+        👋 Hello
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630
+    }
+  );
+}
