d9e455a79e71e2562c3259718fb1b2ddbb595ff2
Masaki Inoue <45062199+2RiniaR@users.noreply.github.com>
2024-03-03 07:51:48 +0900

    Footer: リンク切れ直した (#11)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,16 +10,22 @@ export const Footer = () => (
     <div className={styles.copyright}>Copyright © 2024 Rinia All rights reserved.</div>
     <div className={styles.links}>
       <Link className={styles.element} href="/">
-        Home
+        ホーム
       </Link>
       <ExternalLink className={styles.element} href={`https://x.com/${twitterId}`}>
-        Twitter
+        X(Twitter)
       </ExternalLink>
       <ExternalLink className={styles.element} href="https://mail.google.com/mail/?view=cm&to=7rinia@gmail.com">
-        Mail
+        メール
       </ExternalLink>
-      <ExternalLink className={styles.element} href="">
+      <ExternalLink className={styles.element} href="https://www.youtube.com/@2riniar">
         YouTube
+      </ExternalLink>
+      <ExternalLink className={styles.element} href="https://github.com/2RiniaR">
+        GitHub
+      </ExternalLink>
+      <ExternalLink className={styles.element} href="https://rinear.net/">
+        RineaRの筏
       </ExternalLink>
     </div>
   </footer>
