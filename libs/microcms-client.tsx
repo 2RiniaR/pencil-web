f5e3439cad0d67590092eab6f46d0aeaac6c7b05
Masaki Inoue <45062199+2RiniaR@users.noreply.github.com>
2024-03-02 22:46:20 +0900

    Article: スタイルを見やすく変更 (#6)

--- libs/microcms-client.tsx
+++ libs/microcms-client.tsx
@@ -1,6 +1,7 @@
 import { load } from "cheerio";
 import hljs from "highlight.js";
 import "highlight.js/styles/hybrid.css";
+import styles from "~/templates/Article.module.scss";
 
 export const formatRichText = (richText: string) => {
   const $ = load(richText);
@@ -13,6 +14,14 @@ export const formatRichText = (richText: string) => {
       return hljs.highlightAuto(text).value;
     }
   };
+
+  $("div").each((_, elm) => {
+    const fileName = $(elm).attr("data-filename");
+    if (fileName !== undefined) {
+      $(elm).addClass(styles.fileRoot);
+      $(elm).prepend(`<span class="${styles.filename}">${fileName}</span>`);
+    }
+  });
 
   $("pre code").each((_, elm) => {
     const lang = $(elm).attr("class");
