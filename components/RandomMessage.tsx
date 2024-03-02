f16517fc13c1898e2adab7a65cf96c025df4b54d
watano1168 <watano1168@gmail.com>
2024-03-03 01:19:48 +0900

    Message: クライアントで抽選するように

---
+++ components/RandomMessage.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { chooseRandom } from "~/libs/util";
+
+const messages = [
+  "進捗、どうですか？", // 全体攻撃がしたかった
+  "にゃーん", // エンジニアはみんな猫らしい
+  "この世界完全に理解した", // ここでは私が世界の神なので
+  "今日も生きててえらい！", // エライさんから引用 https://www.rinear.net/materials/marvelous
+  "み　て　る　よ", // みてるよから引用 https://www.rinear.net/materials/eyes
+  "それね、ちょっと分かる", // 創造主botから引用 https://github.com/approvers/king-server
+  "れーざーびーむ！", // メイジシミュレータから引用 https://www.rinear.net/materials/mage-simulator
+  "よく眠る。突然しゃべる。", // Riniaの自己紹介文から引用
+  "遠くへ行こう", // RineaRの筏から引用 https://rineat.net/
+  "nice security, isn't it? :D" // isOddAPIから引用 https://isoddapi.studio.site/
+];
+
+export const RandomMessage = () => {
+  const [message, setMessage] = useState<string>("...");
+
+  useEffect(() => {
+    setMessage(chooseRandom(messages));
+  }, []);
+
+  return <>{message}</>;
+};
