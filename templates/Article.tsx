1d18de19600f574e4b72a3712b877fb90f93e136
Masaki Inoue <45062199+2RiniaR@users.noreply.github.com>
2024-03-10 20:57:06 +0900

    Common: 記事に色を追加 (#20)

--- templates/Article.tsx
+++ templates/Article.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { ArticleContent } from "~/libs/microcms";
+import { ArticleContent, getTypeColor } from "~/libs/microcms";
 import { PageLayout } from "~/templates/PageLayout";
 import styles from "~/templates/Article.module.scss";
 import { formatDisplayDate } from "~/libs/datetime";
@@ -17,12 +17,12 @@ export const Article = ({ article }: Props) => (
       <Link href="/" className={styles.back}>
         {"<"} 一覧へ
       </Link>
-      <div className={styles.heading}>
+      <div className={styles.heading} style={{ borderLeft: `4px solid ${getTypeColor(article.type)}` }}>
         <div className={styles.title}>{article.title}</div>
         <div className={styles.date}>{formatDisplayDate(article.publishedAt)}</div>
-        <div className={styles.share}>
-          <ArticleShare article={article} />
-        </div>
+      </div>
+      <div className={styles.share}>
+        <ArticleShare article={article} />
       </div>
       <div className={styles.body} dangerouslySetInnerHTML={{ __html: formatRichText(article.body) }}></div>
       <div className={styles.about}>
