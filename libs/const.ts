0f465c6ed8d5731926101eff1b24bcd7ade1ec33
Masaki Inoue <45062199+2RiniaR@users.noreply.github.com>
2025-08-24 20:41:55 +0900

    Share: ハッシュタグを入れる (#55)

--- libs/const.ts
+++ libs/const.ts
@@ -4,3 +4,4 @@ export const siteName = "りにあの自由時間";
 export const siteDescription =
   "Rinia（りにあ）の趣味ブログです。技術的なことやアイデアを中心に、好きなことを雑多に書きます。";
 export const twitterId = "2RiniaR";
+export const twitterHashTag = "りにあの自由時間";
