734ceb82a641da94ea6455eb6841812953678587
2riniar <watano1168@gmail.com>
2025-08-26 02:29:42 +0900

    Article: パフォーマンスを改善

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Metadata } from "next";
+import "node_modules/destyle.css/destyle.css";
 import "styles/default.scss";
-import Head from "next/head";
 import { siteDescription, siteName, siteUrl, twitterId } from "~/libs/const";
 
 export const metadata: Metadata = {
@@ -32,22 +32,6 @@ type Props = {
 
 const RootLayout = ({ children }: Props) => (
   <html lang="ja">
-    <Head>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(d) {
-              var config = {
-                kitId: 'srj4vnb',
-                scriptTimeout: 3000,
-                async: true
-              },
-              h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\\bwf-loading\\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
-            })(document);
-          `
-        }}
-      />
-    </Head>
     <body>{children}</body>
   </html>
 );
